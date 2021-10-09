8adabe41caf435a6b6b558e3b260e6ee687f8b1c
caiobarretta <caioabarretta@gmail.com>
2021-10-09 18:40:20 -0300

    Execptions

---
+++ src/Dieta-Ja/src/core/exception/InvalidTypeException.java
@@ -0,0 +1,35 @@
+package core.exception;
+
+public class InvalidTypeException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTypeException() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public InvalidTypeException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTypeException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTypeException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidTypeException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
